054558b2e7c0dbcc8ad41d1a6039b74e77a15268
Jose Vinicius <jviniciusoliveira@gmail.com>
2019-02-27 17:39:48 -0300

    Commit inicial

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,25 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+        this._data = new Date(data.getTime()); // 'Programação Defensiva' - Conceito para evitar alteração da propriedade do Objeto.
+        this._quantidade = quantidade; // A utilização do 'underline' é apenas uma convesão do mundo javascript, para identificar que o atributo é apenas "leitura".
+        this._valor = valor;
+        Object.freeze(this); // Congelamento do Objeto para que nenhuma propriedade seja alterada.
+    }
+
+    get data() {
+        return new Date(this._data.getTime()); // 'Programação Defensiva' - Conceito para evitar alteração da propriedade do Objeto.
+    }
+    
+    get quantidade() {
+        return this._quantidade;
+    }
+    
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
