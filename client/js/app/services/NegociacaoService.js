6720941a77f83e811c7b17b91d183f625c7fc102
Jose Vinicius <jviniciusoliveira@gmail.com>
2019-03-08 18:02:14 -0300

    Refatorando..

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -6,31 +6,32 @@ class NegociacaoService {
 
     obterNegociacoesSemana() {
 
-        return new Promise((resolve, reject) => {
-
-            this._http.get('/negociacoes/semana')
-                .then(negociacoes => resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))))
-                .catch(erro => reject('Falha na importação'));
-        });
+        return this._http
+                .get('/negociacoes/semana')
+                .then(negociacoes => { return negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)) })
+                .catch(erro => { 
+                    console.log(erro);
+                    throw new Error('Falha na importação.');
+                });
     }
 
     obterNegociacoesSemanaAnterior() {
 
-        return new Promise((resolve, reject) => {
-
-            this._http.get('/negociacoes/anterior')
-                .then(negociacoes => resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))))
-                .catch(erro => reject('Falha na importação'));
-        });
+        return this._http.get('/negociacoes/anterior')
+                .then(negociacoes => { return negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)) })
+                .catch(erro => {
+                    console.log(erro);
+                    throw new Error('Falha na importação.');
+                });
     }
 
     obterNegociacoesSemanaRetrasada() {
 
-        return new Promise((resolve, reject) => {
-
-            this._http.get('/negociacoes/retrasada')
-                .then(negociacoes => resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))))
-                .catch(erro => reject('Falha na importação'));
-        });
+        return this._http.get('/negociacoes/retrasada')
+                .then(negociacoes => { return negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)) })
+                .catch(erro => {
+                    console.log(erro);
+                    throw new Error('Falha na importação.');
+                });
     }
 }
